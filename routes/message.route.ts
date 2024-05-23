a71e4cdfffcdd3e9bb62584323ff48964019911e
unknown <sonvip802@gmail.com>
2024-05-23 22:56:46 +0700

    home/23-5

--- routes/message.route.ts
+++ routes/message.route.ts
@@ -3,11 +3,14 @@ import { verifyToken } from "../middlewares/verifyToken";
 import {
   createMessage,
   deleteMessage,
+  getLatestMessage,
   getMessage,
 } from "../controllers/message.controller";
 
 const routerMessage = express.Router();
 routerMessage.route("/createMessage").post(verifyToken, createMessage);
 routerMessage.route("/getMessage").post(verifyToken, getMessage);
+routerMessage.route("/getLatestMessage").get(verifyToken, getLatestMessage);
 routerMessage.route("/deleteMessage").delete(verifyToken, deleteMessage);
+
 export default routerMessage;
